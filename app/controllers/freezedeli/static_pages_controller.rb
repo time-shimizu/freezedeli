491457f66e8852ebde63dfc3314a9eebc9ddf21f
taimushimizu <213.f.r48132@gmail.com>
2020-02-16 11:06:23 +0900

    aboutページの実装

--- app/controllers/freezedeli/static_pages_controller.rb
+++ app/controllers/freezedeli/static_pages_controller.rb
@@ -1,6 +1,9 @@
 class Freezedeli::StaticPagesController < ApplicationController
   def index
     taxons = "弁当"
-    @products = Spree::Product.in_taxons(taxons).all.includes(master: [:images, :default_price]).sample(4)
+    @products = Spree::Product.in_taxons(taxons).includes(master: [:images, :default_price]).sample(4)
+  end
+
+  def about
   end
 end
