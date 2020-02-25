9daa37cd46a7f5d076e716cdff6ce5f2d7ca3475
taimushimizu <213.f.r48132@gmail.com>
2020-02-25 10:12:43 +0900

    Configure Paperclip to use S3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.paperclip_defaults = {
+    storage: :s3,
+    preserve_files: true,
+    s3_host_name: 's3-ap-northeast-1.amazonaws.com',
+    s3_credentials: {
+      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      s3_region: 'ap-northeast-1'
+    },
+    bucket: 'freezedeli'
+  }
 end
